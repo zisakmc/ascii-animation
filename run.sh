5280b6960b7a2bfe3193081b4cb5fe5926c2964f
zisakmc <mishaela98@gmail.com>
2022-01-23 14:59:20 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+banner Lets ride!
+sleep 2s
+clear
+sl
+clear
+banner  Hello world! 
+sleep 3s
+banner This year is about to end before that waifu times hahahahah
+
+sleep 4s
+clear
+
+
+for file in ~/ascii_animation/img/*
+
+do
+
+	jp2a --color-depth=24 -f --clear $file
+	sleep 2s
+	clear
+done
+clear
+banner Thanks For watching welcome to the matrix
+sleep 4s
+clear
+banner you ready!!
+sleep 3s
+cmatrix
+
